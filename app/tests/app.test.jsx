47265fefd51365cf05e66aa4505557d6edbc1016
dougwells <dougwells@gmail.com>
2016-06-09 07:31:17 -0600

    Add automatic tests

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,8 @@
+var expect = require('expect');
+
+//Describe is strictly a visul fn.  Formats output so more readable
+describe('App', function(){
+  it("should properly run test", function(){
+    expect(1).toBe(1);
+  });
+});
